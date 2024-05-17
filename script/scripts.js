d192bab83ab78c8dcd44854db919b03de7e3d129
Alex Bezrodnyi <o.bezrodnyi@gmail.com>
2024-05-17 16:22:23 +0200

    Final Release v2.5

--- script/scripts.js
+++ script/scripts.js
@@ -1,20 +1,23 @@
-$('.owl-carousel').owlCarousel({ 
-    autoplay:true,
-    autoplayTimeout: 2500,
-    autoplaySpeed: 1000,
-    loop: true,
-    margin: 50,
-    nav: true,
-    center:true,
-    responsive:{
-                0:{
-                    items:1
-                },
-                500: {
-                    items:2
-                },
-                1100:{
-                    items:8
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({ 
+        autoplay:true,
+        autoplayTimeout: 2500,
+        autoplaySpeed: 1000,
+        loop: true,
+        margin: 50,
+        nav: true,
+        dots: true,
+        
+        responsive:{
+                    0:{
+                        items:1
+                    },
+                    500: {
+                        items:2
+                    },
+                    1100:{
+                        items:6
+                    }
                 }
-            }
-        });
+            });
+  });
